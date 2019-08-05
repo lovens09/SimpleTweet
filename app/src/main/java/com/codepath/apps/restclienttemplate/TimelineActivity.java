e6bbe76e8def3b97fd81c1c14489fd6a36eeb416
leonlovensky <leonlovenskyll@gmail.com>
2019-08-05 08:00:55 -0400

    Showing the data with a recycle view

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,26 +1,44 @@
 package com.codepath.apps.restclienttemplate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
 
     private TwitterClient client;
-
+    private RecyclerView rvtTweets;
+    private TweetsAdapter adapter;
+    private List<Tweet> tweets;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
         client = TwitterApp.getRestClient(this);
+
+        //find the recycle view
+        rvtTweets = findViewById(R.id.rvTweets);
+
+        //Initialize list of tweets and adapter from the data source
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(this, tweets);
+        //recycle view setup: layout manager and setting the adapter
+        rvtTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvtTweets.setAdapter(adapter);
         populateHomeTimeline();
     }
 
@@ -28,7 +46,21 @@ public class TimelineActivity extends AppCompatActivity {
         client.getHomeTimeline(new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.d("TwitterClient", response.toString());
+            //    Log.d("TwitterClient", response.toString());
+                //Itterate throught the list of tweets
+                for(int i = 0; i<response.length(); i++){
+                    try {
+                        // convert each Json Object into a tweet Object
+                        JSONObject jsonTweetObject = response.getJSONObject(i);
+                        Tweet tweet = Tweet.fromJson(jsonTweetObject);
+                        // Add the tweet into our data source
+                        tweets.add(tweet);
+                        // notify adapeter
+                        adapter.notifyItemInserted(tweets.size() - 1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
